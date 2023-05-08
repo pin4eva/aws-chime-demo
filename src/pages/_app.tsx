c063d350e4c53579af6adc7846fe014969bea813
Peter Akaliro <pin4eva@hotmail.com>
2023-05-08 12:02:59 +0100

    init

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,21 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import {
+  MeetingProvider,
+  lightTheme,
+} from "amazon-chime-sdk-component-library-react";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import React from "react";
+import "../styles/index.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <MeetingProvider>
+        <Component {...pageProps} />
+      </MeetingProvider>
+    </ThemeProvider>
+  );
+};
+
+export default App;
